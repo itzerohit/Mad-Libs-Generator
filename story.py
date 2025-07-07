9a89fb85816585e059e559fdb8154be962771ce7
Rohit Kumar <152129671+itzerohit@users.noreply.github.com>
2025-07-07 20:04:59 +0530

    Add files via upload

---
+++ story.py
@@ -0,0 +1,8 @@
+Today I went to the zoo with my <person>. First, we saw a(n) <adjective> <animal> jumping up and down in its tree. It <verb (past tense)> through the large tunnel that led to its <adjective> <place>.
+
+I was so <emotion>, I couldn’t believe my <body part>! Later, we went to get a <food> and a <drink>. I spilled it all over my <clothing item>.
+
+After that, we saw a group of <plural noun> doing a dance while singing <song>. It was the most <adjective> thing I had ever seen.
+
+On our way home, we <verb (past tense)> all the way back. It was a(n) <adjective> day!
+
